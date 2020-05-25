030bdd7d950f655f0cd247817f08e5791ce92c84
gustavo.fernandez <gustavo.fernandez@fravega.com.ar>
2020-05-25 14:22:42 -0300

    configurar acceso cors

--- src/main/java/com/jendrix/udemy/facuracion/api/auth/ResourceServerConfig.java
+++ src/main/java/com/jendrix/udemy/facuracion/api/auth/ResourceServerConfig.java
@@ -1,10 +1,19 @@
 package com.jendrix.udemy.facuracion.api.auth;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableResourceServer
@@ -13,12 +22,34 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers(HttpMethod.GET, "/api/v1/customers", "/api/v1/customers/regions", "/api/v1/customers/image/**").permitAll()
-				//.antMatchers(HttpMethod.GET, "/api/v1/customers/{id}").hasAnyRole("USER","ADMIN")
-				//.antMatchers(HttpMethod.POST, "/api/v1/customers/upload").hasAnyRole("USER","ADMIN")
-				//.antMatchers(HttpMethod.POST, "/api/v1/customers").hasAnyRole("ADMIN")
-				//.antMatchers("/api/v1/customers/{id}").hasRole("ADMIN")
-				.anyRequest().authenticated();
+				.antMatchers(HttpMethod.GET, "/api/v1/customers", "/api/v1/customers/regions", "/api/v1/customers/image/**", "/images/**")
+				.permitAll()
+				// .antMatchers(HttpMethod.GET, "/api/v1/customers/{id}").hasAnyRole("USER","ADMIN")
+				// .antMatchers(HttpMethod.POST, "/api/v1/customers/upload").hasAnyRole("USER","ADMIN")
+				// .antMatchers(HttpMethod.POST, "/api/v1/customers").hasAnyRole("ADMIN")
+				// .antMatchers("/api/v1/customers/{id}").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				.and().cors().configurationSource(corsConfigurationSource());
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		final FilterRegistrationBean<CorsFilter> bean =  new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
